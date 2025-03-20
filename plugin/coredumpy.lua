02a4c81530baadab34f5981c5dd52e219e30b300
Davidyz <hzjlyz@gmail.com>
2025-03-20 13:21:14 +0000

    fix: strip spaces around the path/url.

--- plugin/coredumpy.lua
+++ plugin/coredumpy.lua
@@ -1,3 +1,3 @@
 vim.api.nvim_create_user_command("Coredumpy", function(args)
-  require("coredumpy").run(args.fargs[1])
+  require("coredumpy").run(args.fargs[1]:gsub("^%s*(.-)%s*$", "%1"))
 end, { complete = "file", nargs = 1 })
